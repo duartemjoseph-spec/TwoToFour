62bb074bccf5f23646e70bf1eb37be16ac674bd5
Juicebox_joe <DuartemJoseph@gmail.com>
2026-01-09 15:45:56 -0800

    Update assembly information and project directory paths

--- obj/Debug/net9.0/TwoToFour.AssemblyInfo.cs
+++ obj/Debug/net9.0/TwoToFour.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TwoToFour")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a0253ef417683e2dc12ee7f8797ce2b26d385c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24c109b7185a9b91e709a73462489466a82c186b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TwoToFour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TwoToFour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
